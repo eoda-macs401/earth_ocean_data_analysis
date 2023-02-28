393272aae810d332f9030e588aa7549fe47fbbaa
eoda-macs401 <emily.roland@wwu.edu>
2023-02-28 12:43:43 -0800

    Time Series

---
+++ _build/jupyter_execute/Assignments/project_overview.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Final Project Readme Assignment - Due 1 Week Before Final Project Due date
+# 
+# To prepare for our [Final Project](Pages/projects.md) submissions, prepare the Project Overview page that will be published as the Readme file to go along with yoru GitHub repository that holds your final project notebook and datasets. 
+# 
+# ## At least 1 Week Prior to Final Project Due dates:
+# 
+# 1. Create a local and remote (hosted on GitHub) repository for your final project, with an appropriate final project name. 
+# 2. Within your final project folder on your computer, create a Readme.md that will serve as your final project front page, and overview of what you've done.  
+# 3. Populate your Readme file with the following information:
+# - Your project title and your name
+# - **Goals** Your project data analysis goal, science question or hypothesis.  
+# - **Motivation** Some amount of background or motivation for your project, to give your audience (me, or another scientists interested in exploring your project) context.
+# - **Data** An overview of the datasets you're using and a brief description of how youre accessing them.  Include whether your data is accessed from a web portal or included in your GitHub Repository.
+# - **Alalysis Methods** An overview of the operations and analysis tasks you will incoroprate in your project.
+# - **Optionally Take-aways or Conclusions** If you have lerned new things from your data or from the process, include them here. 
+# - **References*
+# 
+# 4. Make sure that your Readme is pushed to GitHub.  Invite the instructor (RolandOcean) to be a collaborator on your GitHub Repostiroy for your Final Project. 
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/Assignments/time_series.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Optional Assignment 10: Time Series Play
+# 
+# 1. Find a time series (via a web portal, check out the following options):
+# 
+# - IRIS Seismic Data: http://service.iris.edu/irisws/timeseries/1/
+# - NOAA Buoy Data: https://www.ndbc.noaa.gov/realtime.shtml
+# - Stream Guage Data: https://waterdata.usgs.gov/nwis/uv
+# - Atmospheric CO2: https://gml.noaa.gov/ccgg/trends/data.html
+
+# 2. Import the Data into a Python Interpreter
+
+# In[ ]:
+
+
+
+
+
+# 3. Clean up the data and make sure it is a relatively well behaved time series. Then plot it. 
+
+# In[ ]:
+
+
+
+
+
+# 4. Apply either a spectral analysis or detrend the data!
+
+# In[ ]:
+
+
+
+
